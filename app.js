0a4f9ee898cc3014b09d80be2f96fb8181211558
saltlifegoodlife <saltlifegoodlife@gmail.com>
2023-03-23 11:50:33 -0500

    finished exercises

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       let potentialFail = Math.round(Math.random() * 100) < 10;
+      console.log(potentialFail);
       if (potentialFail) {
         reject({ success: false, message: "Failed to get list of hobbits." });
       } else {
@@ -14,9 +15,23 @@ function getList() {
     }, 10);
   });
 }
-
+let p = document.getElementById("error");
+let ul = document.getElementById("list");
+console.log(p, ul);
 // TODO: Handle the resolved or rejected states of the promise
-
+getList()
+  .then((hobbits) => {
+    console.log(hobbits);
+    hobbits.forEach((e) => {
+      let li = document.createElement("li");
+      li.textContent = e;
+      ul.appendChild(li);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    p.textContent = err.message;
+  });
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
 
